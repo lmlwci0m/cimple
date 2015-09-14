b7ef761de8ab204495adb80799fa573b0f6683cf
Roberto <greatswell@gmail.com>
2015-09-15 00:19:49 +0200

    sequence works

--- src/sequence.c
+++ src/sequence.c
@@ -4,10 +4,13 @@
  
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_SCANF_LEN 4096
 
 int main(int argc, char * argv[]) {
+
+  char value;
 
   char * scanf_input;
   int nchars;
@@ -38,6 +41,22 @@ int main(int argc, char * argv[]) {
    * Deallocation of resources
    */
   free(scanf_input);
+
+  value = 1;
+
+  /*
+   * 0x00 00000000
+   * 0x01 00000001
+   * 0x02 00000010
+   * 0x04 00000100
+   * 0x08 00001000
+   * 0xF0 00010000
+   */
+
+  value = value & 0x0;
+
+  printf("%d %x\n", 256, 255);
+  printf("%d \n", 0x0f);
   
   return EXIT_SUCCESS;
   
